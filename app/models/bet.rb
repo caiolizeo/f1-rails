77a2ab3549c8683f1bcd5c5855979889db16d3de
Caio <caiolizeo@outlook.com>
2022-05-23 14:23:51 -0300

    inicia logica dos pontos da aposta

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -5,14 +5,47 @@ class Bet < ApplicationRecord
 
   def self.update_all
     resp = Faraday.get('http://ergast.com/api/f1/current/last/results.json')
-    resp_result = JSON.parse(resp.body)['MRData']['RaceTable']['Races'][0]['Results']
-    circuit_id = JSON.parse(resp.body)['MRData']['RaceTable']['Circuit']['circuitId']
+    circuit_id = JSON.parse(resp.body)['MRData']['RaceTable']['Races'][0]['Circuit']['circuitId']
     circuit_season = JSON.parse(resp.body)['MRData']['RaceTable']['season']
-
     bets = Bet.where("year = '#{circuit_season}' and circuit = '#{circuit_id}' and validated = false")
-
     return nil if bets.empty?
+    
+    dvr = FormulaOneDriver.where("year = '#{circuit_season}'")
+    drivers = {}
 
+    dvr.each do |d|
+      drivers[d.code] = nil
+    end
+
+    resp_result = JSON.parse(resp.body)['MRData']['RaceTable']['Races'][0]['Results']
+
+    resp_result.each do |r|
+      drivers[r['Driver']['code']] = r['position']
+    end
+
+    puts ' '
+    puts "=== VALIDANDO #{bets.length} APOSTA ===" if bets.length == 1
+    puts "=== VALIDANDO #{bets.length} APOSTAS ===" if bets.length > 1
+    puts ' '
+    puts "Circuito: #{circuit_id}"
+    puts "Temporada: #{circuit_season}"
+    puts ' '
+
+    bets.each do |bet|
+      points = 0
+      points += (1 - drivers[bet.first].to_i).abs
+      points += (2 - drivers[bet.second].to_i).abs
+      points += (3 - drivers[bet.third].to_i).abs
+      points += (4 - drivers[bet.fourth].to_i).abs
+      points += (5 - drivers[bet.fifth].to_i).abs
+      points += (6 - drivers[bet.sixth].to_i).abs
+      points += (7 - drivers[bet.seventh].to_i).abs
+      points += (8 - drivers[bet.eighth].to_i).abs
+      points += (9 - drivers[bet.ninth].to_i).abs
+      points += (10 - drivers[bet.tenth].to_i).abs
+
+      puts points
+    end
     # criar logica de pontuação
     # ===============================
     # pole: 20pts
