1a13d1120a3482748cf852846b656fcce16e37a5
Caio Lizeo <caiolizeo@outlook.com>
2022-11-07 23:35:46 -0300

    add nome sobrenome e username no devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :devise_custom_params, if: :devise_controller?
+
+  protected
+
+  def devise_custom_params
+    signup_params = [:first_name, :last_name, :username, :email]
+    update_params = [:first_name, :last_name, :email]
+    devise_parameter_sanitizer.permit(:sign_up, keys: signup_params)
+    devise_parameter_sanitizer.permit(:account_update, keys: update_params)
+  end
 end
