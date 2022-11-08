7bbda6b70cdd2b0629cde974f6375c2356f02cf5
Caio Lizeo <caiolizeo@outlook.com>
2022-11-08 18:45:10 -0300

    adiciona apostas no seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_191336) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_015237) do
   create_table "bet_points", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "points"
@@ -88,8 +88,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_29_191336) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "bet_points", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+F1Driver.update
+F1Circuit.update
+
+driver_codes = []
+
+F1Driver.all.each do |driver|
+  driver_codes << driver.code
+end
+
 50.times do
   first_name = Faker::Name.first_name
   last_name  = Faker::Name.last_name
@@ -20,10 +30,32 @@
     puts "First name: #{user.first_name}"
     puts "Last name:  #{user.last_name}"
     puts "Username:   #{user.username}"
-    puts "Password:   #{user.password}"
+    puts "Password:   #{user.password}\n\n"
+  end
+
+  driver_codes = driver_codes.shuffle
+  bet = Bet.new(user: user,
+                circuit: F1Circuit.last_race.code,
+                year: F1Circuit.last_race.year,
+                pole: driver_codes[0],
+                first: driver_codes[1],
+                second: driver_codes[2],
+                third: driver_codes[3],
+                fourth: driver_codes[4],
+                fifth: driver_codes[5],
+                sixth: driver_codes[6],
+                seventh: driver_codes[7],
+                eighth: driver_codes[8],
+                ninth: driver_codes[9],
+                tenth: driver_codes[10],
+                result_points: nil,
+                validated: "invalidated")
+
+  if bet.save
+    puts "Bet created for user #{user.username}"
     puts "======================================="
   end
+
 end
 
-#F1Driver.update
-#F1Circuit.update
+Bet.update_all
